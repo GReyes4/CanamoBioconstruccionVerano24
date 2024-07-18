8011bfa1892c22951d7baadb941cf047e1964cdd
Skalitz <a01198666@tec.mx>
2024-07-18 16:23:06 -0600

    added icons + responsive changes

--- front/src/pages/CanamoConecta/CanamoConecta.jsx
+++ front/src/pages/CanamoConecta/CanamoConecta.jsx
@@ -24,13 +24,40 @@ function CanamoConecta() {
                 </div>
 
                 <div className='rightSide'>
-                  <img src="\src\assets\images\Flechas2.png" alt="Flechas1" />
+                  <img src="\src\assets\images\Flechas2.png" alt="Flechas2" />
                 </div>
               </div>
 
-              <p>Una iniciativa para conectar diversos sectores como la <strong>academia, investigadores,
+              <p>
+              <br /> 
+              Una iniciativa para conectar diversos sectores como la <strong>academia, investigadores,
               laboratorios, empresarios, funcionarios públicos, estudiantes</strong> y el público interesado
-              en la industria del cáñamo en México. </p>
+              en la industria del cáñamo en México. 
+              <br /> <br />
+
+              Esta conexión es crucial para el desarrollo de la industria, ya que la falta de
+              <strong> integración y comunicación</strong> ha limitado su avance.
+              <br /> <br />
+              </p>
+
+              <div className='arrowContainer'>
+                <div className='leftSide'>
+                 <p>Nuestra plataforma facilita la colaboración <strong>interdiscipinaria</strong>, estableciendo bases
+                  y criterios que guían el desarrollo del <strong>cáñamo</strong> industrial con estándares de <strong>calidad,
+                  seguridad y sostenibilidad.</strong></p>
+                </div>
+
+                 <img src="\src\assets\images\Stickmen.png" alt="Stickmen" className='icon'/>
+              </div>
+
+              <div className='arrowContainer'>
+              <img src="\src\assets\images\Bullseye.png" alt="Bullseye" className='icon'/>
+                <div className='leftSide'>
+                  <p>Al enfocarse exclusivamente en el cáñamo industrial, evita controversias relacionadas
+                  con el uso lúdico del cannabis, aumentando su aceptación y apoyo. </p>
+                </div>
+              </div>
+
             </div>
 
           </div>
