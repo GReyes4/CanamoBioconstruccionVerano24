faeaab3a64d5e83a0f45055195af0e20840c55a0
Skalitz <a01198666@tec.mx>
2024-07-18 15:06:05 -0600

    added arrows

--- front/src/pages/CanamoConecta/CanamoConecta.jsx
+++ front/src/pages/CanamoConecta/CanamoConecta.jsx
@@ -12,6 +12,28 @@ function CanamoConecta() {
           </div>
 
           <span className='subtitle'> Conoce más acerca de nuestra misión, proyectos, y cómo puedes ayudar</span>
+
+          <div className='cañamoContent'>
+
+            <div className='container'>
+
+              <div className='arrowContainer'>
+                <div className="leftSide">
+                  <img src="\src\assets\images\Flechas1.png" alt="Flechas1" />
+                  <h2>¿De qué trata?</h2>
+                </div>
+
+                <div className='rightSide'>
+                  <img src="\src\assets\images\Flechas2.png" alt="Flechas1" />
+                </div>
+              </div>
+
+              <p>Una iniciativa para conectar diversos sectores como la <strong>academia, investigadores,
+              laboratorios, empresarios, funcionarios públicos, estudiantes</strong> y el público interesado
+              en la industria del cáñamo en México. </p>
+            </div>
+
+          </div>
         
         </div>
       </>
