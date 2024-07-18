43e4954bf20df33720c4513a5eed7dab2c8f91ec
Skalitz <a01198666@tec.mx>
2024-07-18 13:01:16 -0600

    added Montserrat font + changes in canamoConecta

--- front/src/pages/CanamoConecta/CanamoConecta.jsx
+++ front/src/pages/CanamoConecta/CanamoConecta.jsx
@@ -1,10 +1,21 @@
+import './CanamoConecta.css'
+
+
 function CanamoConecta() {
-  
     return (
       <>
-        <h1>Canamo Conecta</h1>
+        <div className='CanamoPage'>
+          <div className='backgroundImage'>
+            <div className='overlay'>
+              <h1>Cáñamo Conecta</h1>
+            </div>
+          </div>
+
+          <span className='subtitle'> Conoce más acerca de nuestra misión, proyectos, y cómo puedes ayudar</span>
+        
+        </div>
       </>
     )
 }
-  
+
 export default CanamoConecta
