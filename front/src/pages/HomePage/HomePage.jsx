0ee7a06774fb46f6b175635e6713a372f481c24a
GReyes4 <105297867+GReyes4@users.noreply.github.com>
2024-07-19 02:05:34 -0600

    Imagen Encabezado y Redes

--- front/src/pages/HomePage/HomePage.jsx
+++ front/src/pages/HomePage/HomePage.jsx
@@ -1,5 +1,12 @@
 import Navbar from '../../GlobalComponents/Navbar/Navbar.jsx';
 
+import iconoFacebook from '../Proyecto/Facebook.png';
+import iconoInsta from '../Proyecto/Instagram.png';
+import iconoTwitter from '../Proyecto/Twitter.png';
+import iconoLinkedIn from '../Proyecto/Linkedin.png';
+import iconoYoutube from '../Proyecto/Youtube.png';
+import iconoWhats from '../Proyecto/whatsapplogo.png';
+
 import './HomePage.css'
 
 function HomePage() {
@@ -7,6 +14,17 @@ function HomePage() {
     return (
       <>
       <Navbar />
+      <div className="encabezado-div" >
+        <div className='redes-sociales-div'>
+          <img src={iconoFacebook} className='social-icon'/>
+          <img src={iconoInsta} className='social-icon'/>
+          <img src={iconoTwitter} className='social-icon'/>
+          <img src={iconoLinkedIn} className='social-icon'/>
+          <img src={iconoYoutube} className='social-icon'/>
+          <img src={iconoWhats} className='social-icon'/>
+        </div>
+      </div>
+
 
       </>
     )
