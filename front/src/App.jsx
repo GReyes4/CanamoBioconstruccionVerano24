588142002a2c271023bb75b41bc6cb36c1a728c9
Kaled Enríquez <kaledtrejo@hotmail.com>
2024-07-24 12:51:20 -0600

    alianzas + cañamo

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -17,12 +17,16 @@ import CanamoConecta from "./pages/CanamoConecta/CanamoConecta.jsx";
 // Proyecto
 import Proyecto from "./pages/Proyecto/Proyecto.jsx";
 
+//Alianzas
+import Alianzas from "./pages/Alianzas/Alianzas.jsx";
+
 const router = createBrowserRouter(
   createRoutesFromElements(
       <Route path="/" element={<Root />}>
         <Route index element={<HomePage />} />
         <Route path="canamo" element={<CanamoConecta />} />
         <Route path="proyecto" element={<Proyecto />} />
+        <Route path="alianzas" element={<Alianzas />} />
         
       </Route>
   )
