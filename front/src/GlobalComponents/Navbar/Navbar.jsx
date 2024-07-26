24712516a4b5dae687f4dcfeaeb4db05fd090dfb
GReyes4 <105297867+GReyes4@users.noreply.github.com>
2024-07-25 22:30:41 -0600

    Responsividad en navbar

--- front/src/GlobalComponents/Navbar/Navbar.jsx
+++ front/src/GlobalComponents/Navbar/Navbar.jsx
@@ -1,6 +1,7 @@
-import {Navbar, NavbarBrand, NavbarContent, NavbarItem, DropdownItem, DropdownTrigger, Dropdown, DropdownMenu, Link as NextLink} from "@nextui-org/react";
+import {Navbar, NavbarBrand, NavbarContent, NavbarItem, DropdownItem, DropdownTrigger, Dropdown, DropdownMenu, Link as NextLink, NavbarMenuToggle, NavbarMenu, NavbarMenuItem} from "@nextui-org/react";
 import { useNavigate, Link } from "react-router-dom";
 import logoAMEEC from '../../assets/ameec_logo.png';
+import logoAMEECMobile from '../../assets/AmeecLogoCircular.png';
 
 import './Navbar.css'
 
@@ -10,9 +11,10 @@ function NavBar() {
     return (
       <>
       <div className="navbar-wrapper">
-        <Navbar className="navbar-container" >
+        <Navbar className="navbar-container" isBordered>
         <NavbarBrand className="navbar-brand">
-          <img src={logoAMEEC} alt="AMEEC Logo" className="logo-img" onClick={() => navigate("/")}/>
+          <img src={logoAMEEC} alt="AMEEC Logo" className="logo-img desktop-logo" onClick={() => navigate("/")} />
+          <img src={logoAMEECMobile} alt="AMEEC Logo" className="logo-img mobile-logo" onClick={() => navigate("/")} />
         </NavbarBrand>
 
         <NavbarContent className="navbar-content-center gap-12" justify="center">
@@ -78,13 +80,65 @@ function NavBar() {
           </NavbarItem>
         </NavbarContent>
 
-        <NavbarContent className="navbar-content-end" justify="end">
+        <NavbarContent className="navbar-content-end" justify="end" hideIn="xs">
           <NavbarItem>
             <button className="donar-button" onClick={() => navigate("/donaciones")}>
               Donar
             </button>
           </NavbarItem>
         </NavbarContent>
+
+        {/* Botón del menú de hamburguesa para móviles */}
+        <NavbarMenuToggle className="menu-hamburguesa" aria-label="Toggle navigation" />
+
+        {/* Menú lateral de hamburguesa para móviles */}
+        <NavbarMenu>
+            <NavbarMenuItem>
+              <NextLink className="navbar-link text-lg" as={Link} to="/nosotros">
+                Nosotros
+              </NextLink>
+            </NavbarMenuItem>
+            <NavbarMenuItem>
+              <NextLink className="navbar-link text-lg" as={Link} to="/proyecto">
+                Proyecto
+              </NextLink>
+            </NavbarMenuItem>
+            <NavbarMenuItem>
+              <NextLink className="navbar-link text-lg" as={Link} to="/canamo">
+                Cáñamo Conecta
+              </NextLink>
+            </NavbarMenuItem>
+            <NavbarMenuItem>
+              <NextLink className="navbar-link text-lg" as={Link} to="/naayeri">
+                Naayeri
+              </NextLink>
+            </NavbarMenuItem>
+            <NavbarMenuItem>
+              <NextLink className="navbar-link text-lg" as={Link} to="/alianzas">
+                Alianzas
+              </NextLink>
+            </NavbarMenuItem>
+            <NavbarMenuItem>
+              <NextLink className="navbar-link text-lg" as={Link} to="/galeria">
+                Galería de fotos
+              </NextLink>
+            </NavbarMenuItem>
+            <NavbarMenuItem>
+              <NextLink className="navbar-link text-lg" as={Link} to="/donaciones">
+                Presupuesto/Donaciones
+              </NextLink>
+            </NavbarMenuItem>
+            <NavbarMenuItem>
+              <NextLink className="navbar-link text-lg" as={Link} to="/preguntas">
+                Preguntas frecuentes
+              </NextLink>
+            </NavbarMenuItem>
+            <NavbarMenuItem>
+              <button className="donar-button-movil" onClick={() => navigate("/donaciones")}>
+                Donar
+              </button>
+            </NavbarMenuItem>
+          </NavbarMenu>
 
         </Navbar>
       </div>
